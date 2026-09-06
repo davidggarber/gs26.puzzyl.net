43b8df750e0c83be2942518828567e97c991a3b8
David Garber <davidggarber@hotmail.com>
2026-09-06 00:05:15 -0700

    Bank of New England, part 1

--- src/index_data.js
+++ src/index_data.js
@@ -42,6 +42,7 @@ function buildIndexOfPuzzles() {
     { round: 0, title: 'Four Crosses',                          thumb: '', author: 'Martyn Lovell',       type: logic,    group: puzzle, orientation: portrait,   cls:'', feeder: [] },
     { round: 0, title: 'Zero, One Two Many!',                   thumb: '', author: 'Glenn Hollingsworth', type: logic,    group: puzzle, orientation: portrait,   cls:'', feeder: [] },
     { round: 0, title: 'Big Ideas',                             thumb: '', author: 'Martyn Lovell',       type: word,     group: puzzle, orientation: landscape,  cls:'', feeder: [] },
-    { round: 0, title: 'Mad Scientist Store',                   thumb: '', author: 'Martyn Lovell',       type: word,     group: puzzle, orientation: landscape,  cls:'', feeder: [] },
+    { round: 0, title: 'Mad Scientist Store',                   thumb: '', author: 'Martyn Lovell',       type: word,     group: puzzle, orientation: portrait,   cls:'', feeder: [] },
+    { round: 0, title: 'Bank of New England',                   thumb: '', author: 'Martyn Lovell',       type: word,     group: puzzle, orientation: landscape,  cls:'', feeder: [] },
   ];
 }
